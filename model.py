471507645d1b3504d32200d2219c62bde0bef768
Kevin Yap <me@kevinyap.ca>
2018-04-13 11:38:41 -0700

    Improve `CreditModel` documentation

--- model.py
+++ model.py
@@ -3,16 +3,31 @@ import numpy as np
 
 class CreditModel:
     def __init__(self):
-        """Initializes the model object."""
-        # TODO: Initialize any class objects/variables you might need
+        """
+        Instantiates the model object, creating class variables if needed.
+        """
+
+        # TODO: Initialize your model object.
         pass
 
-    def fit(self, X, y):
-        """Fits the model based on the given X and y."""
-        # TODO: Fit your model
+    def fit(self, X_train, y_train):
+        """
+        Fits the model based on the given `X_train` and `y_train`.
+
+        You should somehow manipulate and store this data to your model class
+        so that you can make predictions on new testing data later on.
+        """
+
+        # TODO: Fit your model based on the given X and y.
         pass
 
-    def predict(self, X):
-        """Returns y_hat for a given X, after fitting."""
-        # TODO: Return a prediction for your model
-        return np.random.randint(2, size=len(X))
+    def predict(self, X_test):
+        """
+        Returns `y_hat`, a prediction for a given `X_test` after fitting.
+
+        You should make use of the data that you stored/computed in the
+        fitting phase to make your prediction on this new testing data.
+        """
+
+        # TODO: Predict on `X_test` based on what you learned in the fit phase.
+        return np.random.randint(2, size=len(X_test))
