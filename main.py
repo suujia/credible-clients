e417cca5a49844146bde51556d85efc57d47c44a
Kevin Yap <me@kevinyap.ca>
2018-04-12 22:03:55 -0700

    Add skeleton for ML pretask

---
+++ main.py
@@ -0,0 +1,26 @@
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.dummy import DummyClassifier
+
+
+def load_data(path, seed=42):
+    data = np.loadtxt(path, dtype=np.int, delimiter=',', skiprows=1)
+    return train_test_split(data[:, 1:-1], data[:, -1], random_state=seed)
+
+
+def main():
+    # Load data from disk and split into training and validation sets.
+    X_train, X_test, y_train, y_test = load_data('data/credit-data.csv')
+
+    # TODO: Replace this dummy classifier with your own model!
+    model = DummyClassifier(strategy='uniform')
+    model.fit(X_train, y_train)
+
+    # Print out the model accuracy after predicting against `X_test`.
+    y = model.predict(X_test)
+    accuracy = np.sum(y == y_test) / y.size
+    print("Model accuracy: {:.6f}".format(accuracy))
+
+
+if __name__ == '__main__':
+    main()
