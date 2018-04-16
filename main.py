cc8b7cb5ad7fbe4558689bd46bf7c6ec601d0e34
Kevin Yap <me@kevinyap.ca>
2018-04-15 20:09:43 -0700

    Improve model runner script
    - Print precision and recall scores along with model accuracy.
    - Ensure `y_hat` returns integer values, rather than floats.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, precision_score, recall_score
 
 from model import CreditModel
 
@@ -10,14 +11,19 @@ def main():
     X, y = data[:, 1:-1], data[:, -1]
     X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42)
 
-    # Fit the model against training data, and print its accuracy on test data.
+    # Fit the model against the training data.
     model = CreditModel()
     model.fit(X_train, y_train)
 
+    # Predict against test data and ensure `y_hat` returns ints.
     y_hat = model.predict(X_test)
+    y_hat = np.rint(np.squeeze(y_hat)).astype(int)
     assert len(y_hat) == len(X_test)
-    accuracy = np.sum(np.squeeze(y_hat) == y_test) / float(y_test.size)
-    print("Model accuracy: {:.3f}%".format(accuracy * 100))
+
+    # Print out accuracy/precision/recall scores.
+    print("Accuracy:  {:06.3f}%".format(100 * accuracy_score(y_test, y_hat)))
+    print("Precision: {:06.3f}%".format(100 * precision_score(y_test, y_hat)))
+    print("Recall:    {:06.3f}%".format(100 * recall_score(y_test, y_hat)))
 
 
 if __name__ == '__main__':
